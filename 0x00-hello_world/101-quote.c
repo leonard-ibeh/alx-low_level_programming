7dee4b9e8c44fdf04bfe1608e0e178ae7b82372c
leonard-ibeh <obinnaibeh42@gmail.com>
2022-12-09 05:21:36 -0800

    0x00 C- Hello, world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,38 @@
 #include <unistd.h>
 
 
-
 /**
- * main - print exactly and that piece of art is useful
  *
- * Return: Always 0 (Success)
- */
+ *  *  * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *
+ *   *   *                followed by a new line, to standard error.
+ *
+ *    *    *
+ *
+ *     *     * Return: Always 1.
+ *
+ *      *      */
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (1);
+
+	<<<<<<< HEAD
+
+				write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",  59);
+
+
+
+			return (1);
+
+			=======
+
+				write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+
+						sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+
+			return (1);
+
+			>>>>>>> b55b0d73a75321aa0a9ba600ae7aec14be9793ea
+
 }
