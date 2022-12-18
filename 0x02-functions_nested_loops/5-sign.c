89af58918f158f6add8c51dc13ac5122e59bd832
leonard-ibeh <obinnaibeh42@gmail.com>
2022-12-17 19:49:24 -0800

    updated 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,6 +7,7 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
@@ -16,7 +17,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		putchar('0');
+		_putchar('0');
 		return (0);
 	}
 	else
