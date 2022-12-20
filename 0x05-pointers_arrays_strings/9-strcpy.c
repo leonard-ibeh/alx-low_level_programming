d1c06516835c6c9b29602227f0deb4065db2aef9
leonard-ibeh <obinnaibeh42@gmail.com>
2022-12-20 05:12:01 -0800

    update 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strcpy - copy the string pointed to by scr to dest
+ * @dest: char to check
+ * @scr: char to check
+ * Return: 0 is success
+ */
+char *_strcpy(char *dest, char *scr)
+{
+	int a;
+
+	for (a = 0; scr[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+
+	return (dest);
+}
