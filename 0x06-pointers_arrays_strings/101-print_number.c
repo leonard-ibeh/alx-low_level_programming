6bffb87e402d7c7a5c5bff95ddafcdb0720727bc
Ibeh leonard <116468589+leonard-ibeh@users.noreply.github.com>
2022-12-23 00:54:58 +0000

    update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- * print_number - Prints any integer with putchar
- * @n: Number to prints
- *
- * Return: Nothing
- */
+  * print_number - Prints any integer with putchar
+  * @n: Number to prints
+  *
+  * Return: Nothing
+  */
 void print_number(int n)
 {
 	unsigned int x;
 
-	if (n < 0);
+	if (n < 0)
 	{
 		_putchar('-');
 		n *= -1;
