37f3f9255864833c6c7c7c5fd637d37f427f5db5
leonard-ibeh <obinnaibeh42@gmail.com>
2023-01-05 04:53:38 -0800

    updated 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@
 int prime_check(int f, int p)
 {
 	if (p < 2 || p % f == 0)
-		return (o);
+		return (0);
 	else if (f > p / 2)
 		return (1);
 	else
@@ -35,3 +35,4 @@ int is_prime_number(int n)
 		return (1);
 	return (prime_check(2, n));
 }
+
