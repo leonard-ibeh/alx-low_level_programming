3ee414a779aed3e123cd4d96ab85c5e8abb1089c
leonard-ibeh <obinnaibeh42@gmail.com>
2023-01-20 01:06:05 -0800

    updated 3-get_op_func.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,23 +14,23 @@
 
 int main(int argc, char *argv[])
 {
-	int (*func_ptr)(int, int);
+	int a, b, c;
+	int (*f)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(99);
+		exit(98);
 	}
-
-	func_ptr = get_op_func(argv[2]);
-
-	if (func_ptr == NULL)
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	f = get_op_func(argv[2]);
+	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	printf("%d\n", func_ptr(atoi(aargv[1]), atoi(argv[3])));
-
+	c = f(a, b);
+	printf("%d\n", c);
 	return (0);
 }
